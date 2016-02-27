4ef00ba0c4c3684f54afbac86f5a126557001369
charpi_m <mathilde.charpiot@epitech.eu>
2016-02-27 12:13:26 +0100

    aff_game

--- src/game/aff_game.c
+++ src/game/aff_game.c
@@ -5,12 +5,29 @@
 ** Login   <lefevr_h@epitech.net>
 ** 
 ** Started on  Sat Feb 27 11:51:50 2016 Lefevre Philippe
-** Last update Sat Feb 27 11:59:13 2016 Lefevre Philippe
+** Last update Sat Feb 27 12:12:55 2016 mathilde charpiot
 */
 
 #include		"sudoki.h"
 
-void			aff_map(int **map)
+void    affichage(int   tab[9][9])
 {
-  (void)map;
+  int   i;
+  int   j;
+
+  i = 0;
+  j = 0;
+  while(i < 9)
+    {
+      while(j < 9)
+        {
+          printf(((j + 1)% 3) ? "%d " : "%d|", tab[i][j]);
+          j++;
+        }
+      my_putchar('\n');
+      if(!((i + 1)% 3)
+         my_putstr("------------------");
+	 i++;
+	 }
+      my_putstr("\n\n");
 }
