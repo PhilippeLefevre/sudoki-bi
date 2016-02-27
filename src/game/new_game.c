f9246c7fc4715be47b67e5512751158511edf553
Lefevre Philippe <philippe1.lefevre@epitech.eu>
2016-02-27 16:53:11 +0100

    default

--- src/game/new_game.c
+++ src/game/new_game.c
@@ -5,56 +5,13 @@
 ** Login   <lefevr_h@epitech.net>
 **
 ** Started on  Sat Feb 27 11:50:14 2016 Lefevre Philippe
-** Last update Sat Feb 27 16:35:22 2016 Lefevre Philippe
+** Last update Sat Feb 27 16:53:06 2016 Lefevre Philippe
 */
 
 #include		<stdio.h>
 #include		<string.h>
 #include		"sudoki.h"
 
-static void		map_error(char *buf)
-{
-  free(buf);
-  exit(write(2, "MAP ERROR\n\n", 11));
-}
-
-int			fill_map(int map[9][9])
-{
-  char			*buf;
-  int			x;
-  int			y;
-
-  y = 0;
-  while ((buf = get_next_line(0)) != NULL)
-    {
-      if (!(y % 10))
-	{
-	  if (strcmp(buf, "|------------------|") != 0)
-	    map_error(buf);
-	}
-      else
-	{
-	  x = 0;
-	  while (x < 20)
-	    {
-	      if (!(x % 19))
-		if (buf[x] != '|')
-		  map_error(buf);
-		else
-		  x += 1;
-	      else if (x % 2)
-		x += 1;
-	      map[(y - 1)][(((x + 1) / 2) - 1)] = ((buf[x] == ' ') ? (0) : (buf[x] - 48));
-	      x += 1;
-	    }
-	}
-      y += 1;
-      free(buf);
-    }
-  if (y != 11)
-    exit(write(2, "MAP ERROR\n\n", 11));
-  return (0);
-}
 
 int			absentSurLigne(int k, int grille[9][9], int i)
 {
@@ -91,21 +48,35 @@ int			absentSurBloc(int k, int grille[9][9], int i, int j)
 
   _i = i - (i % 3);
   _j = j - (j % 3);
-  for (i=_i; i < _i+3; i++)
-    for (j=_j; j < _j+3; j++)
-      if (grille[i][j] == k)
-	return (0);
+  i = _i;
+  while  (i < (_i + 3))
+    {
+      j = _j;
+      while (j < _j + 3)
+	{
+	  if (grille[i][j] == k)
+	    return (0);
+	  j += 1;
+	}
+      i += 1;
+    }
   return (1);
 }
 
 int			estValide(int grille[9][9], int position)
 {
+  int			i;
+  int			j;
+  int			k;
+
   if (position == 9 * 9)
     return (1);
-  int i = position / 9, j = position % 9;
+  i = position / 9;
+  j = position % 9;
   if (grille[i][j] != 0)
     return estValide(grille, position + 1);
-  for (int k = 1; k <= 9; k++)
+  k = 1;
+  while (k <= 9)
     {
       if (absentSurLigne(k, grille, i) && absentSurColonne(k, grille, j) && absentSurBloc(k, grille, i, j))
 	{
@@ -113,8 +84,64 @@ int			estValide(int grille[9][9], int position)
 	  if (estValide(grille, position + 1))
     	    return (1);
 	}
+      k += 1;
     }
   grille[i][j] = 0;
+  return (0);
+}
+
+static void		map_error(char *buf)
+{
+  free(buf);
+  exit(write(2, "MAP ERROR\n\n", 11));
+}
+
+int			fill_map(int map[9][9])
+{
+  char			*buf;
+  int			x;
+  int			y;
+  int			z;
+
+  y = 0;
+  z = 0;
+  while ((buf = get_next_line(0)) != NULL)
+    {
+      if (z && !y)
+	printf("####################\n");
+      if (!(y % 10))
+	{
+	  if (strcmp(buf, "|------------------|") != 0)
+	    map_error(buf);
+	}
+      else
+	{
+	  x = 0;
+	  while (x < 20)
+	    {
+	      if (!(x % 19))
+		if (buf[x] != '|')
+		  map_error(buf);
+		else
+		  x += 1;
+	      else if (x % 2)
+		x += 1;
+	      map[(y - 1)][(((x + 1) / 2) - 1)] = ((buf[x] == ' ') ? (0) : (buf[x] - 48));
+	      x += 1;
+	    }
+	}
+      y += 1;
+      /* printf("== [%s]\n", buf); */
+      free(buf);
+      if (y == 11)
+	{
+	  y = 0;
+	  z += 1;
+	  aff_game(map, estValide(map, 0));
+	}
+    }
+  /* if (y != 11) */
+  /*   exit(write(2, "MAP ERROR\n\n", 11)); */
   return (0);
 }
 
@@ -123,6 +150,5 @@ int			new_game()
   int			map[9][9];
 
   fill_map(map);
-  aff_game(map, estValide(map, 0));
   return (0);
 }
