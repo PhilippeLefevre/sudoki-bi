ba5cc487e1856d6905cc44ce012bbad0f7cb6239
Lefevre Philippe <philippe1.lefevre@epitech.eu>
2016-02-28 01:26:13 +0100

    correction size get_next_line better speed

--- inc/get_next_line.h
+++ inc/get_next_line.h
@@ -5,7 +5,7 @@
 ** Login   <lefevr_h@epitech.net>
 **
 ** Started on  Tue Jan  5 02:47:02 2016 Lefevre Philippe
-** Last update Fri Jan 22 15:50:30 2016 Lefevre Philippe
+** Last update Sun Feb 28 01:24:34 2016 Lefevre Philippe
 */
 
 #ifndef			GET_NEXT_LINE_H_
@@ -15,7 +15,7 @@
 # include		<errno.h>
 
 # ifndef		READ_SIZE
-#  define		READ_SIZE	(14)
+#  define		READ_SIZE	(21)
 # endif			/* !READ_SIZE */
 
 char			*my_realloc(char *cur, int n);
